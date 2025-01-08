33a4e250d0890d65c08e00c89ce6094fced2f2b8
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-01-08 14:43:51 +0900

    fix : add vpc outputs

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,18 @@ output "network_self_link" {
   description = "The self-link of the VPC network"
   value       = google_compute_network.main.self_link
 }
+
+output "subnetworks" {
+  description = "The list of subnetworks associated with the VPC"
+  value       = google_compute_network.main.subnetworks
+}
+
+output "routing_mode" {
+  description = "The routing mode of the VPC network"
+  value       = google_compute_network.main.routing_mode
+}
+
+output "gateway_ip" {
+  description = "The gateway IP address of the VPC network"
+  value       = google_compute_network.main.gateway_ipv4
+}
